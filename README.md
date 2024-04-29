ce151bca0bd2802ee4986f3294600153626f7d1e
Michael O'Connor <oconnor.michael.stephen@gmail.com>
2024-04-28 22:23:44 -0400

    feat: add astrovim and customizations

---
+++ README.md
@@ -0,0 +1,25 @@
+# Nvim Config
+
+## 🛠️ Installation
+
+#### Make a backup of your current nvim and shared folder
+
+```shell
+mv ~/.config/nvim ~/.config/nvim.bak
+mv ~/.local/share/nvim ~/.local/share/nvim.bak
+mv ~/.local/state/nvim ~/.local/state/nvim.bak
+mv ~/.cache/nvim ~/.cache/nvim.bak
+```
+
+#### Clone the repository
+
+```shell
+git clone https://github.com/oconnormi/nvim ~/nvim
+ln -s ~/nvim ~/.config/nvim
+```
+
+#### Start Neovim
+
+```shell
+nvim
+```
---
+++ lua/community.lua
@@ -0,0 +1,27 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  -- import/override with your plugins folder
+  { import = "astrocommunity.colorscheme.neosolarized-nvim"},
+
+  -- Test Support
+  { import = "astrocommunity.test.neotest" },
+  { import = "astrocommunity.test.nvim-coverage" },
+
+  -- Language Packs
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.helm" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.yaml"},
+
+  -- Navigation
+  { import = "astrocommunity.motion.leap-nvim" }
+}
